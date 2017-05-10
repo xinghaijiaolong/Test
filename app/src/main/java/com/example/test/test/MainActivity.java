eb07428a0825a62d0f82b0ffafd244b1bcf745ee
xinghaijiaolong <2633933315@qq.com>
2017-05-10 16:11:07 +0800

    7

--- app/src/main/java/com/example/test/test/MainActivity.java
+++ app/src/main/java/com/example/test/test/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         int a=5;
+        int b=7;
     }
 }
